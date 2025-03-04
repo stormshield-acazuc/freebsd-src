3b5c928d4ca197cbcf9c4a9429714bd5cc10ef9c
acazuc <acazuc@acazuc.fr>
2025-03-04 11:18:14 +0100

    pmc: add emerald rapids model
    Sponsored By: Stormshield

--- sys/dev/hwpmc/hwpmc_intel.c
+++ sys/dev/hwpmc/hwpmc_intel.c
@@ -244,6 +244,10 @@ pmc_intel_initialize(void)
 			cputype = PMC_CPU_INTEL_ATOM_TREMONT;
 			nclasses = 3;
 			break;
+		case 0xCF:
+			cputype = PMC_CPU_INTEL_EMERALD_RAPIDS;
+			nclasses = 3;
+			break;
 		}
 		break;
 	}
